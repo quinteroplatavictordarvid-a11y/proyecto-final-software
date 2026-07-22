0e1aeaf983c1a15ee586c76fde42a2e03abeaee7
Victor Quintero <vdquinterop@utn.edu.ec>
2026-07-22 18:37:22 -0500

    conexion local

--- src/main/java/ec/edu/utn/golmundial/estadisticas/config/DatabaseConfig.java
+++ src/main/java/ec/edu/utn/golmundial/estadisticas/config/DatabaseConfig.java
@@ -6,12 +6,12 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
 
-//    private static final String URL = "jdbc:postgresql://localhost:5432/GolMundial2026";
-//    private static final String USER = "postgres";
-//    private static final String PASSWORD = "postgres123";
-    private static final String URL = "jdbc:postgresql://postgres:DPGLZxXJhmelcZDYkezzaWRAgcGUpPDH@sakura.proxy.rlwy.net:56826/railway";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "DPGLZxXJhmelcZDYkezzaWRAgcGUpPDH";
+   private static final String URL = "jdbc:postgresql://localhost:5432/GolMundial2026";
+   private static final String USER = "postgres";
+   private static final String PASSWORD = "postgres123";
+    // private static final String URL = "jdbc:postgresql://postgres:DPGLZxXJhmelcZDYkezzaWRAgcGUpPDH@sakura.proxy.rlwy.net:56826/railway";
+    // private static final String USER = "postgres";
+    // private static final String PASSWORD = "DPGLZxXJhmelcZDYkezzaWRAgcGUpPDH";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
